f86f8a89b1c58b8dd7232a547620c5de6a5eca16
MD.Shimul <md.shimuldiu@gmail.com>
2025-09-20 12:09:46 +0600

    json modiy conversion added

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let product=require("./Product_order.json");
-console.log(product);
+/*console.log(product);
 console.log(product.address.country);
 console.log(product.items[0].price);
-console.log(product.address.road)
+console.log(product.address.road) */
+//================== conversation===============
+console.log(JSON.parse('{"Name":"shimul"}'));
+let student={
+    name:"shimul",
+    roll:9,
+    cgpa:34.5
+}
+console.log(JSON.stringify(student));
