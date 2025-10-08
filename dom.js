dbb2e49f4c0c6c53ececd00b054e5b7ef240cd79
MD.Shimul <miashimul6363@gmail.com>
2025-10-08 08:18:27 +0600

    added dom

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
-/* document.getElementById("b").innerHTML= "shimul";
+ /* document.getElementById("b").innerHTML= "shimul";
 let change=document.getElementsByTagName("h1")[0];
 change.innerHTML= "mustafa";
 document.getElementsByTagName("P")[0].innerHTML= "rifat"; */
-let ImAGE=document.getElementsByClassName("image")[0];
+/*let ImAGE=document.getElementsByClassName("image")[0];
 function sh(){
     ImAGE.src="Images/shimul.jpg";
 }
@@ -13,4 +13,11 @@ let riyan=document.querySelector("#riyan");
 riyan.innerHTML="Rifat & Riyan";
 function RIFAT(){
     ImAGE.src="Images/rifat.jpg"
+} */
+let pera=document.getElementById("p")
+function ad(){
+   pera.classList.add("sty");
+}
+function remov(){
+   pera.classList.remove("sty");
 }
