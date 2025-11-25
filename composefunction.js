e24965908755200e94f86160941c3b4cb6a7dd4a
MD.Shimul <miashimul6363@gmail.com>
2025-11-25 23:41:46 +0600

    leetcode problem 2629 solved

---
+++ composefunction.js
@@ -0,0 +1,12 @@
+var compose = function(functions) {
+    return function(x) {
+        for (let i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x);
+        }
+        return x;
+    }
+};
+let functions = [x => x + 1, x => x * x, x => 2 * x]
+let x = 4
+let fn=compose(functions)
+console.log(fn(4))
