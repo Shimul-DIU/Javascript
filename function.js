beaeaceff7a031cbb7c030aed6a0d0320a85c1f9
MD.Shimul <miashimul6363@gmail.com>
2025-11-26 12:35:15 +0600

    excercise functions type

--- function.js
+++ function.js
@@ -33,3 +33,29 @@ let numbers=[2,4,5,3,4]
 for (let number of numbers){
     console.log(number)
 }
+// ===============IIFE==========
+(x=>{
+    x+1
+    console.log('hello')
+})()
+// ============callback function=============
+let FunctionExpresion=function Myfunction(x,y,callback){
+    let sum=x+y
+    callback(sum)
+}
+function callafter(result){
+    console.log(`result is :${result}`)
+}
+FunctionExpresion(2,3,callafter)
+// =============== Heiger order functions=========
+function outer(x,y,inputfuncton){
+    inputfuncton(x,y)
+}
+function inner1(a,b){
+    console.log('first function of sum is :',a+b)
+}
+function inner2(c,d){
+    console.log('second function of sum is :',c*d)
+}
+outer(2,5,inner1)
+outer(2,5,inner2)
