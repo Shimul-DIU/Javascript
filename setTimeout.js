2bd601eccc91de85621db4b341cf2bd3e20df001
MD.Shimul <miashimul6363@gmail.com>
2025-12-02 16:38:36 +0600

    modify setinterval

--- setTimeout.js
+++ setTimeout.js
@@ -1,3 +1,8 @@
-setInterval(() => {
-   console.log('hi') 
+let num=4
+let Setinterval=setInterval(() => {
+   console.log(num) 
+   num++;
+   if (num==11){
+    clearInterval(Setinterval)
+   }
 }, 4000);
