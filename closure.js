31698d745921adc867ef54af1afe18ebccb9dfd0
MD.Shimul <md.shimuldiu@gmail.com>
2026-01-21 23:07:45 +0600

    onceCall in closure

--- closure.js
+++ closure.js
@@ -8,3 +8,23 @@ function Outer(){
 }
 let fn=Outer()
 console.log(fn(10))
+
+// another example
+function PrintOnce(){
+    let count=0
+    return function(){
+        if (count==0){
+            count++
+            console.log('print once')
+        }
+        else{
+            
+            console.log("undefined")
+        }
+        
+    }
+}
+let obj=PrintOnce()
+obj()
+obj()
+obj()
